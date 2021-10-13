539dd5a730bd1091a0ae1a3bc7e392f7596ac235
Bonnie RH Bao <72422943+rbao23@users.noreply.github.com>
2021-10-13 16:30:52 -0700

    Update pgpass.R
    Hide password for database connection in public version

--- code_base/pgpass.R
+++ code_base/pgpass.R
@@ -1,2 +1,3 @@
-Sys.setenv(PGPASSWORD="mphBnQUw1soooxsIf5n6")  # PostgreSQL password
+# password is needed in the pgpass.R file for database connection
+Sys.setenv(PGPASSWORD="************")  # PostgreSQL password
 
