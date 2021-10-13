ba4169cbde776378cae44f4346b92cd27dab8a54
rbao23 <ruihab@uw.edu>
2021-10-13 15:19:26 -0700

    fixed database connection issue

--- app.R
+++ app.R
@@ -14,19 +14,19 @@ library(dplyr)
 library(RPostgreSQL)
 library(magrittr)
 library(tidyverse)
+library(leaflet)
 
 #library(shinydashboard)
 
 ################## Getting data from the database e#############################################
 
 # script to connect
-source("code_base/dbconnect.R")
-# password env var
-source("code_base/pgpass.R")
+source("H:/ruihab/Documents/BBF-Social-Weather-Dashboard/code_base/dbconnect.R")
+source("H:/ruihab/Documents/BBF-Social-Weather-Dashboard/code_base/pgpass.R")
 
 # create a connection to the postgresql database
 # note that "con" will be used later in each connection to the database
-con <- connectDB(host = "bffsw.csde.washington.edu", dbname = "ruihab_sb")
+con <- connectdb(host = "bffsw.csde.washington.edu", dbname = "ruihab_sb")
 
 # test
 dbListTables(conn = con) 
@@ -64,50 +64,48 @@ body<-navbarPage(theme = shinytheme("flatly"), collapsible = TRUE,
                  title = "Social Weather Community Well-Being Dashboard",
                  tabPanel("Comparion Map",h2("Select Dataset"),
                           sidebarLayout(
-                              sidebarPanel(
-                                  selectInput("domain","Domain:",choices=sort(unique(social_index_dataset$domain))),
-                                  selectInput("subdomain", "Subdomain:", choices=NULL),
-                                  selectInput("indicator", "Indicator:", choices=NULL),
-                                  selectInput("sex", "Sex:",choices=NULL),
-                                  selectInput("race", "Race:",choices=NULL),
-                                  selectInput("age", "Age:",choices=NULL),
-                                  selectInput("year", "Year:",choices=NULL)
-                              ),
-                              mainPanel(
-                                  tabsetPanel(
-                                      tabPanel("US Map View", verbatimTextOutput("mapview"),
-                                               fluidRow(column(11, wellPanel(tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar{
+                            sidebarPanel(
+                              selectInput("domain","Domain:",choices=sort(unique(social_index_dataset$domain))),
+                              selectInput("subdomain", "Subdomain:", choices=NULL),
+                              selectInput("indicator", "Indicator:", choices=NULL),
+                              selectInput("sex", "Sex:",choices=NULL),
+                              selectInput("race", "Race:",choices=NULL),
+                              selectInput("age", "Age:",choices=NULL),
+                              selectInput("year", "Year:",choices=NULL)
+                            ),
+                            mainPanel(
+                              tabsetPanel(
+                                tabPanel("US Map View", verbatimTextOutput("mapview"),
+                                         fluidRow(column(11, wellPanel(tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar{
                                                      background: #48C9B0;
                                                      border-top: 1px solid #48C9B0 ; border-bottom: 1px solid #48C9B0}")),
-                                                                             sliderInput("one", "Select Mapping Year", value =1990, min = 1990, max=2021, step=1,animate=TRUE,ticks=TRUE),
-                                                                             leafletOutput("mymap"),
-                                                                             p(),
-                                                                             actionButton("recalc", "New points")
-                                               )))),
-                                      tabPanel("State Profile View", verbatimTextOutput("summary")),
-                                      tabPanel("County Profile View", tableOutput("table1"),
-                                               fluidRow(column(8, wellPanel(
-                                                   tags$style(HTML(".js-irs-1 .irs-single, .js-irs-1 .irs-bar-edge, .js-irs-1 .irs-bar{
+                                                                       sliderInput("yearslider", "Select Mapping Year", value =1990, min = 1990, max=2021, step=1,round = TRUE, animate=TRUE),
+                                                                       leafletOutput("mymap"),
+                                                                       h6("Welcome to the Social Weather Map! Use the left panel to filter data, and click on the map for additional details. Please note that data are not currently available for every county in every year, and estimates will change as we process more data.")
+                                         )))),
+                                tabPanel("State Profile View", verbatimTextOutput("stateview")),
+                                tabPanel("County Profile View", tableOutput("countyview"),
+                                         fluidRow(column(8, wellPanel(
+                                           tags$style(HTML(".js-irs-1 .irs-single, .js-irs-1 .irs-bar-edge, .js-irs-1 .irs-bar{
                                                      background: #48C9B0;
                                                      border-top: 1px solid #48C9B0 ; border-bottom: 1px solid #48C9B0}")),
-                                                   sliderInput("two", "Select Mapping Year", value =2021, min = 1990, max=2021, step=1)
-                                               )))),
-                                      tabPanel("Tract Profile View", tableOutput("table2"),
-                                               fluidRow(column(8, wellPanel(
-                                                   tags$style(HTML(".js-irs-2 .irs-single, .js-irs-2 .irs-bar-edge, .js-irs-2 .irs-bar{
+                                           sliderInput("countyview", "Select Mapping Year", value =2021, min = 1990, max=2021, step=1,animate=TRUE)
+                                         )))),
+                                tabPanel("Tract Profile View", tableOutput("tractview"),
+                                         fluidRow(column(8, wellPanel(
+                                           tags$style(HTML(".js-irs-2 .irs-single, .js-irs-2 .irs-bar-edge, .js-irs-2 .irs-bar{
                                                      background: #48C9B0;
                                                      border-top: 1px solid #48C9B0 ; border-bottom: 1px solid #48C9B0}")),
-                                                   sliderInput("three", "Select Mapping Year", value =2021, min = 1990, max=2021, step=1)
-                                               )))),
-                                      tabPanel("Zip Code Profile View", tableOutput("table3"),
-                                               fluidRow(column(8, wellPanel(
-                                                   tags$style(HTML(".js-irs-3 .irs-single, .js-irs-3 .irs-bar-edge, .js-irs-3 .irs-bar{
+                                           sliderInput("tractview", "Select Mapping Year", value =2021, min = 1990, max=2021, step=1,animate=TRUE)
+                                         )))),
+                                tabPanel("Zip Code Profile View", tableOutput("zctaview"),
+                                         fluidRow(column(8, wellPanel(
+                                           tags$style(HTML(".js-irs-3 .irs-single, .js-irs-3 .irs-bar-edge, .js-irs-3 .irs-bar{
                                                      background: #48C9B0;
                                                      border-top: 1px solid #48C9B0 ; border-bottom: 1px solid #48C9B0}")),
-                                                   sliderInput("four", "Select Mapping Year", value =2021, min = 1990, max=2021, step=1)
-                                               ))))
-                                  )
-                              ))), 
+                                           sliderInput("zctaview", "Select Mapping Year", value =2021, min = 1990, max=2021, step=1,animate=TRUE)
+                                         ))))
+                              )))), 
                  tabPanel("About this site",h2("About Social Weather Index"))
 )
 
@@ -115,84 +113,77 @@ body<-navbarPage(theme = shinytheme("flatly"), collapsible = TRUE,
 
 # Define server logic required to draw a histogram
 server <- function(input,output,session) {
-    #reactive selectinput
-    domain <- reactive({
-        print("event domain")
-        req(input$domain)
-        filter(social_index_dataset, domain == input$domain)
-    })
-    observeEvent(domain(), {
-        print("event subdomain")
-        updateSelectInput(session,"subdomain", choices = sort(unique(domain()$subdomain)))
-    })
-    subdomain <- reactive({
-        req(input$subdomain)
-        filter(domain(), subdomain == input$subdomain)
-    })
-    observeEvent(subdomain(), {
-        print("event indicator")
-        updateSelectInput(session,inputId = "indicator", choices = sort(unique(subdomain()$indicator)))
-    })
-    indicator <- reactive({
-        req(input$indicator)
-        filter(subdomain(), indicator == input$indicator)
-    })
-    observeEvent(indicator(), {
-        print("event sex")
-        updateSelectInput(session,inputId = "sex", choices = sort(unique(indicator()$sex)))
-    })
-    sex <- reactive({
-        req(input$sex)
-        filter(indicator(), sex == input$sex)
-    })
-    observeEvent(sex(), {
-        print("event race")
-        updateSelectInput(session,inputId = "race", choices = sort(unique(sex()$race)))
-    })
-    race <- reactive({
-        req(input$race)
-        filter(sex(), race == input$race)
-    })
-    observeEvent(race(), {
-        print("event age")
-        updateSelectInput(session,inputId = "age", choices = sort(unique(race()$age)))
-    })
-    
-    age <- reactive({
-        req(input$age)
-        filter(race(), age == input$age)
-    })
-    
-    observeEvent(age(), {
-        print("event year")
-        updateSelectInput(session,inputId = "year", choices = sort(unique(age()$year)))
-    })
-    
-    year <- reactive({
-        req(input$year)
-        filter(age(), year == input$year)
-    })
-    # reactive sliderinput
-    observeEvent(year(),{
-        updateSliderInput(session, "one", value = input()$year,
-                          min = min(age()$year), max = max(age()$year), step = 1)
-    })
-    
-    # Reactive expression to create data frame of all input values ----
-    points <- eventReactive(input$recalc, {
-      cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-    }, ignoreNULL = FALSE)
-    
-    output$mymap <- renderLeaflet({
-      leaflet() %>%
-        addTiles() %>%
-        setView(lng = -93.85, lat = 37.45, zoom = 4)
-    })
-    
-    
-    
-    
-    
+#reactive selectinput
+domain <- reactive({
+    print("event domain")
+    req(input$domain)
+    filter(social_index_dataset, domain == input$domain)
+  })
+  observeEvent(domain(), {
+    print("event subdomain")
+    updateSelectInput(session,"subdomain", choices = sort(unique(domain()$subdomain)))
+  })
+  subdomain <- reactive({
+    req(input$subdomain)
+    filter(domain(), subdomain == input$subdomain)
+  })
+  observeEvent(subdomain(), {
+    print("event indicator")
+    updateSelectInput(session,inputId = "indicator", choices = sort(unique(subdomain()$indicator)))
+  })
+  indicator <- reactive({
+    req(input$indicator)
+    filter(subdomain(), indicator == input$indicator)
+  })
+  observeEvent(indicator(), {
+    print("event sex")
+    updateSelectInput(session,inputId = "sex", choices = sort(unique(indicator()$sex)))
+  })
+  sex <- reactive({
+    req(input$sex)
+    filter(indicator(), sex == input$sex)
+  })
+  observeEvent(sex(), {
+    print("event race")
+    updateSelectInput(session,inputId = "race", choices = sort(unique(sex()$race)))
+  })
+  race <- reactive({
+    req(input$race)
+    filter(sex(), race == input$race)
+  })
+  observeEvent(race(), {
+    print("event age")
+    updateSelectInput(session,inputId = "age", choices = sort(unique(race()$age)))
+  })
+  
+  age <- reactive({
+    req(input$age)
+    filter(race(), age == input$age)
+  })
+  
+  observeEvent(age(), {
+    print("event year")
+    updateSelectInput(session,inputId = "year", choices = sort(unique(age()$year)))
+  })
+  
+  year <- reactive({
+    req(input$year)
+    filter(age(), year == input$year)
+  })
+  # reactive sliderinput
+  observeEvent(year(),{
+    updateSliderInput(session, "yearslider", value = input$year,
+                      min = min(age()$year), max = max(age()$year), step = 1)
+  })
+  
+  # Reactive expression to create data frame of all input values ----
+  
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(provider = "CartoDB") %>% 
+      setView(lng = -93.85, lat = 37.45, zoom = 4)
+  })
+
 
 }
 
